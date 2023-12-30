ed00ef7ae4af5ef4506570d107081618487d267b
natashazelinka <natashazelinka@googlemail.com>
2023-12-30 22:11:12 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Weather App</p>
-        <a
-          className="App-link"
-          href="https://github.com/natashazelinka/weather-react"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This project was coded 👩‍💻 by Natasha Zelinka
-        </a>
-      </header>
+      <div className="container">
+        <footer>
+          This project was coded by Natasha Zelinka and is{" "}
+          <a
+            href="https://github.com/natashazelinka/weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://app.netlify.com/teams/natashazelinka/builds/659092bf441ae1000875af40"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
