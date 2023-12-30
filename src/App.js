abda6b4e1c81ef39f461f1804d062bc6065a1319
natashazelinka <natashazelinka@googlemail.com>
2023-12-30 21:59:23 +0000

    Added basic text to App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Weather App</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/natashazelinka/weather-react"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This project was coded 👩‍💻 by Natasha Zelinka
         </a>
       </header>
     </div>
