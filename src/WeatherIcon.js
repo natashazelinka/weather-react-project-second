3a8b3e30bf435836aef6352c3190a57127cf93cf
natashazelinka <natashazelinka@googlemail.com>
2024-01-06 19:56:20 +0000

    Changed Icon sizing and added multiple icons

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="lightblue"
-      size={props.size}
+      size={150}
       animate={true}
     />
   );
